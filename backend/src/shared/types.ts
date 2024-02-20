97251839fca96764e0f390fb580b593a38839f13
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-02-21 00:29:26 +0530

    implement  search pages

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -14,6 +14,14 @@ export type HotelType = {
     starRating: number;
     imageUrls: string[];
     lastUpdated: Date;
-  
 
 };
+
+export type HotelSearchResponse = {
+    data: HotelType[];
+    pagination: {
+      total: number;
+      page: number;
+      pages: number;
+    };
+  };
