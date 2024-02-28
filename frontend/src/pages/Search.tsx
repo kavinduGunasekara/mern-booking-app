fa9570a19ace6bb7ff78dc63c4a7e82fff73da10
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-02-28 18:16:57 +0530

    sorting & filtering
    complete sorting and search filtering function

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -6,6 +6,8 @@ import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
 import StarRatingFilter from "../components/StarRatingFilter";
 import HotelTypesFilter from "../components/HotelTypesFilter";
+import FacilitiesFilter from "../components/FacilitiesFilter";
+import PriceFilter from "../components/PriceFilter";
 //import HotelTypesFilter from "../components/HotelTypesFilter";
 //import FacilitiesFilter from "../components/FacilitiesFilter";
 //import PriceFilter from "../components/PriceFilter";
@@ -15,9 +17,9 @@ const Search = () => {
   const [page, setPage] = useState<number>(1);
   const [selectedStars, setSelectedStars] = useState<string[]>([]);
   const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
- // const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
-  //const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
- // const [sortOption, setSortOption] = useState<string>("");
+  const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+  const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
+  const [sortOption, setSortOption] = useState<string>("");
 
   const searchParams = {
     destination: search.destination,
@@ -27,10 +29,10 @@ const Search = () => {
     childCount: search.childCount.toString(),
     page: page.toString(),
     stars: selectedStars,
-   // types: selectedHotelTypes,
-   // facilities: selectedFacilities,
-   //// maxPrice: selectedPrice?.toString(),
-    //sortOption,
+    types: selectedHotelTypes,
+    facilities: selectedFacilities,
+    maxPrice: selectedPrice?.toString(),
+    sortOption,
   };
 
   const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
@@ -46,7 +48,7 @@ const Search = () => {
         : prevStars.filter((star) => star !== starRating)
     );
   };
-  
+
   const handleHotelTypeChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -56,6 +58,16 @@ const Search = () => {
       event.target.checked
         ? [...prevHotelTypes, hotelType]
         : prevHotelTypes.filter((hotel) => hotel !== hotelType)
+    );
+  };
+
+  const handleFacilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const facility = event.target.value;
+
+    setSelectedFacilities((prevFacilities) =>
+      event.target.checked
+        ? [...prevFacilities, facility]
+        : prevFacilities.filter((prevFacility) => prevFacility !== facility)
     );
   };
 
@@ -77,6 +89,16 @@ const Search = () => {
             selectedHotelTypes={selectedHotelTypes}
             onChange={handleHotelTypeChange}
           />
+
+         <FacilitiesFilter
+            selectedFacilities={selectedFacilities}
+            onChange={handleFacilityChange}
+          />
+
+          <PriceFilter
+            selectedPrice={selectedPrice}
+            onChange={(value?: number) => setSelectedPrice(value)}
+          />
          
           
         </div>
@@ -87,6 +109,21 @@ const Search = () => {
             {hotelData?.pagination.total} Hotels found
             {search.destination ? ` in ${search.destination}` : ""}
           </span>
+
+          <select
+            value={sortOption}
+            onChange={(event) => setSortOption(event.target.value)}
+            className="p-2 border rounded-md"
+          >
+            <option value="">Sort By</option>
+            <option value="starRating">Star Rating</option>
+            <option value="pricePerNightAsc">
+              Price Per Night (low to high)
+            </option>
+            <option value="pricePerNightDesc">
+              Price Per Night (high to low)
+            </option>
+          </select>
          
         </div>
         {hotelData?.data.map((hotel) => (
